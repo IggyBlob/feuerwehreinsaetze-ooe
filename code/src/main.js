72a45c8d04cb4fb821c482ca4204433a34c4f81f
IggyBlob <socialmedia@paulhaunschmied.com>
2021-01-28 13:28:39 +0100

    Implement barchart

--- code/src/main.js
+++ code/src/main.js
@@ -61,6 +61,8 @@ d3.select("#month").on("change", function () {
     updateApp();
 });
 
+const alarmTypeHistogram = createHistogram("#alarmTypeHistogram");
+
 function createMap(filteredAlarms) {
     // remove existing paths
     mapElement.selectAll("g").remove();
@@ -108,6 +110,77 @@ function createMap(filteredAlarms) {
         .attr("d", path);
 }
 
+function createHistogram(svgSelector) {
+    const margin = {
+        top: 5,
+        bottom: 200,
+        left: 150,  // CAUTION: if modified, CSS must also be adapted
+        right: 0,
+    };
+    const width = 500 - margin.left - margin.right;
+    const height = 500 - margin.top - margin.bottom;
+
+    // creates sources <svg> element
+    const svg = d3
+        .select(svgSelector)
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom);
+
+    // group used to enforce margin
+    const g = svg
+        .append("g")
+        .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    // scales setup
+    const xscale = d3.scaleBand().rangeRound([0, width]).paddingInner(0.1);
+    const yscale = d3.scaleLinear().range([height, margin.top]);
+
+    // axis setup
+    const xaxis = d3.axisBottom().scale(xscale);
+    const g_xaxis = g.append("g").attr("class", "x axis").attr("transform", `translate(0,${height})`);
+    const yaxis = d3.axisLeft().scale(yscale);
+    const g_yaxis = g.append("g").attr("class", "y axis");
+
+    function update(new_data) {
+        xscale.domain(new_data.map((d) => d.alarmType));
+        yscale.domain([0, d3.max(new_data, (d) => d.count)]);
+
+        g_xaxis.transition().call(xaxis);
+        g_yaxis.transition().call(yaxis);
+
+        const rect = g
+            .selectAll("rect")
+            .data(new_data, (d) => d.alarmType)
+            .join(
+                (enter) => {
+                    const rect_enter = enter.append("rect").attr("y", height);
+                    rect_enter.append("title");
+                    return rect_enter;
+                },
+                (update) => update,
+                (exit) => exit.remove()
+            );
+
+        rect.transition()
+            .attr("x", (d) => xscale(d.alarmType))
+            .attr("y", d => yscale(d.count))
+            .attr("height", d => yscale(0) - yscale(d.count))
+            .attr("width", xscale.bandwidth());
+
+        rect.select("title")
+            .text((d) => d.alarmType);
+
+        // rotate x-axis labels by 45deg
+        svg.selectAll("g.x.axis g text")
+            .style("text-anchor", "end")
+            .attr("dx", "-.8em")
+            .attr("dy", ".15em")
+            .attr("transform", "rotate(-45)");
+    }
+
+    return update;
+}
+
 function groupAlarmsByDistrict(alarms) {
     const map = new Map();
     alarms.forEach(item => {
@@ -126,7 +199,10 @@ function updateApp() {
     const filteredAlarms = filterAlarmsData();
     const filteredBrigades = filterBrigadesData();
 
+    const topAlarmTypes = groupAlarmsByType(filteredAlarms, 10);
+
     createMap(filteredAlarms);
+    alarmTypeHistogram(topAlarmTypes)
 }
 
 function filterAlarmsData() {
@@ -139,4 +215,17 @@ function filterBrigadesData() {
     return state.brigadesData.filter(brigade => {
         return brigade.callStart.month() + 1 === state.month
     });
+}
+
+function groupAlarmsByType(filteredAlarms, n) {
+    const unorderedMap = new Map();
+    filteredAlarms.forEach(item => {
+        const key = item.alarmType;
+        const value = unorderedMap.get(key);
+        unorderedMap.set(key, value ? value + 1 : 1);
+    });
+    return [...unorderedMap]
+        .sort((a, b) => b[1] - a[1])
+        .slice(0, n)
+        .map(a => { return {alarmType: a[0], count: a[1]} });
 }
